18f49ce7036f04feb62f10a5b14fed7e2666315a
Kim Maida <git@kim-maida.com>
2015-05-27 16:57:20 -0400

    FIX #2 - controllerAs in config.

--- public/ng-app/core/app.config.js
+++ public/ng-app/core/app.config.js
@@ -12,17 +12,25 @@
 		$routeProvider
 			.when('/', {
 				templateUrl: 'ng-app/home/Home.view.html',
+				controller: 'HomeCtrl',
+				controllerAs: 'home',
 				secure: true
 			})
 			.when('/login', {
-				templateUrl: 'ng-app/login/Login.view.html'
+				templateUrl: 'ng-app/login/Login.view.html',
+				controller: 'LoginCtrl',
+				controllerAs: 'login'
 			})
 			.when('/account', {
 				templateUrl: 'ng-app/account/Account.view.html',
+				controller: 'AccountCtrl',
+				controllerAs: 'account',
 				secure: true
 			})
 			.when('/admin', {
 				templateUrl: 'ng-app/admin/Admin.view.html',
+				controller: 'AdminCtrl',
+				controllerAs: 'admin',
 				secure: true
 			})
 			.otherwise({
