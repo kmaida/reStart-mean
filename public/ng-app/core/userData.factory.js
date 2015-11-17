c78afcd36418e651282dc021a0d4550a4f48657d
Kim Maida <yi.mihi@gmail.com>
2015-11-17 11:20:23 -0500

    * Change userData service to a factory (styleguide req)

---
+++ public/ng-app/core/userData.factory.js
@@ -0,0 +1,79 @@
+// User API $http calls
+(function() {
+	'use strict';
+
+	angular
+		.module('myApp')
+		.factory('userData', userData);
+
+	userData.$inject = ['$http'];
+
+	function userData($http) {
+		/**
+		 * Promise response function
+		 * Checks typeof data returned and succeeds if JS object, throws error if not
+		 *
+		 * @param response {*} data from $http
+		 * @returns {*} object, array
+		 * @private
+		 */
+		function _successRes(response) {
+			if (typeof response.data === 'object') {
+				return response.data;
+			} else {
+				throw new Error('retrieved data is not typeof object.');
+			}
+		}
+
+		/**
+		 * Promise response function - error
+		 * Throws an error with error data
+		 *
+		 * @param error {object}
+		 * @private
+		 */
+		function _errorRes(error) {
+			throw new Error('Error retrieving data', error);
+		}
+
+		/**
+		 * Get current user's data
+		 *
+		 * @returns {promise}
+		 */
+		function getUser() {
+			return $http
+				.get('/api/me')
+				.then(_successRes, _errorRes);
+		}
+
+		/**
+		 * Update current user's profile data
+		 *
+		 * @param profileData {object}
+		 * @returns {promise}
+		 */
+		function updateUser(profileData) {
+			return $http
+				.put('/api/me', profileData);
+		}
+
+		/**
+		 * Get all users (admin authorized only)
+		 *
+		 * @returns {promise}
+		 */
+		function getAllUsers() {
+			return $http
+				.get('/api/users')
+				.then(_successRes, _errorRes);
+		}
+
+		// callable members
+		return {
+			getUser: getUser,
+			updateUser: updateUser,
+			getAllUsers: getAllUsers
+		}
+	}
+})();
