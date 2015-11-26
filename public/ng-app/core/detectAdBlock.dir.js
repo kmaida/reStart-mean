daea3b37a998e6b1e366dc01b55fc8b25e066fc8
Kim Maida <git@kim-maida.com>
2015-11-26 12:49:54 -0500

    #15 - cleanup detectAdblock directive.

--- public/ng-app/core/detectAdBlock.dir.js
+++ public/ng-app/core/detectAdBlock.dir.js
@@ -7,13 +7,31 @@
 	detectAdblock.$inject = ['$timeout', '$location'];
 
 	function detectAdblock($timeout, $location) {
+		// return directive
+		return {
+			restrict: 'EA',
+			link: detectAdblockLink,
+			template:   '<div class="ad-test fa-facebook fa-twitter" style="height:1px;"></div>' +
+			'<div ng-if="ab.blocked" class="ab-message alert alert-danger">' +
+			'<i class="fa fa-ban"></i> <strong>AdBlock</strong> is prohibiting important functionality! Please disable ad blocking on <strong>{{ab.host}}</strong>. This site is ad-free.' +
+			'</div>'
+		};
 
+		/**
+		 * detectAdblock LINK function
+		 *
+		 * @param $scope
+		 * @param $elem
+		 * @param $attrs
+		 */
 		function detectAdblockLink($scope, $elem, $attrs) {
 			// data object
 			$scope.ab = {};
 
 			// hostname for messaging
 			$scope.ab.host = $location.host();
+
+			$timeout(_areAdsBlocked, 200);
 
 			/**
 			 * Check if ads are blocked - called in $timeout to let AdBlockers run
@@ -25,17 +43,6 @@
 
 				$scope.ab.blocked = _a.height() <= 0 || !$elem.find('.ad-test:visible').length;
 			}
-
-			$timeout(_areAdsBlocked, 200);
-		}
-
-		return {
-			restrict: 'EA',
-			link: detectAdblockLink,
-			template:   '<div class="ad-test fa-facebook fa-twitter" style="height:1px;"></div>' +
-						'<div ng-if="ab.blocked" class="ab-message alert alert-danger">' +
-							'<i class="fa fa-ban"></i> <strong>AdBlock</strong> is prohibiting important functionality! Please disable ad blocking on <strong>{{ab.host}}</strong>. This site is ad-free.' +
-						'</div>'
 		}
 	}
 
