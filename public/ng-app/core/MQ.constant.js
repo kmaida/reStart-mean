e82fddbf95b0f96183021290c0561c8f0c1fd275
Kim Maida <git@kim-maida.com>
2015-11-26 12:51:07 -0500

    * Cleanup constants.

--- public/ng-app/core/MQ.constant.js
+++ public/ng-app/core/MQ.constant.js
@@ -2,10 +2,12 @@
 (function() {
 	'use strict';
 
+	var MQ = {
+		SMALL: '(max-width: 767px)',
+		LARGE: '(min-width: 768px)'
+	};
+
 	angular
 		.module('myApp')
-		.constant('MQ', {
-			SMALL: '(max-width: 767px)',
-			LARGE: '(min-width: 768px)'
-		});
+		.constant('MQ', MQ);
 })();
