a4996cbf825bfe8de012596e439cf60cdcccaf7a
Kim Maida <git@kim-maida.com>
2015-11-26 13:28:37 -0500

    #15, #19 - LoginCtrl bindables up top, _init() procedural code.

--- public/ng-app/login/Login.ctrl.js
+++ public/ng-app/login/Login.ctrl.js
@@ -11,7 +11,24 @@
 		// controllerAs ViewModel
 		var login = this;
 
-		Page.setTitle('Login');
+		// bindable members
+		login.logins = OAUTH.LOGINS;
+		login.loggingIn = false;
+		login.authenticate = _authenticate;
+
+		_init();
+
+		/**
+		 * INIT function executes procedural code
+		 *
+		 * @private
+		 */
+		function _init() {
+			Page.setTitle('Login');
+
+			// get local data
+			localData.getJSON().then(_localDataSuccess);
+		}
 
 		/**
 		 * Function to run when local data successful
@@ -23,42 +40,46 @@
 			login.localData = data;
 		}
 
-		localData.getJSON().then(_localDataSuccess);
-
-		login.logins = OAUTH.LOGINS;
-
 		/**
 		 * Authenticate the user via Oauth with the specified provider
 		 *
 		 * @param {string} provider - (twitter, facebook, github, google)
 		 */
-		login.authenticate = function(provider) {
+		function _authenticate(provider) {
 			login.loggingIn = true;
-
-			/**
-			 * Successfully authenticated
-			 * Go to initially intended authenticated path
-			 *
-			 * @param response {object} promise response
-			 * @private
-			 */
-			function _authSuccess(response) {
-				login.loggingIn = false;
-
-				if ($rootScope.authPath) {
-					$location.path($rootScope.authPath);
-				} else {
-					$location.path('/');
-				}
-			}
 
 			$auth.authenticate(provider)
 				.then(_authSuccess)
-				.catch(function(response) {
-					console.log(response.data);
-					login.loggingIn = 'error';
-					login.loginMsg = ''
-				});
+				.catch(_authCatch);
+		}
+
+		/**
+		 * Successfully authenticated
+		 * Go to initially intended authenticated path
+		 *
+		 * @param response {object} promise response
+		 * @private
+		 */
+		function _authSuccess(response) {
+			login.loggingIn = false;
+
+			if ($rootScope.authPath) {
+				$location.path($rootScope.authPath);
+			} else {
+				$location.path('/');
+			}
+		}
+
+		/**
+		 * Failure authenticating
+		 *
+		 * @param response {object}
+		 * @private
+		 */
+		function _authCatch(response) {
+			console.log(response.data);
+			login.loggingIn = 'error';
+			login.loginMsg = ''
 		}
 	}
 })();
