564a81c237c3704da4e6392ef9aaad45f8620e93
Kim Maida <git@kim-maida.com>
2015-11-26 12:46:49 -0500

    FIX #18 - update Page.factory to setTitle and getTitle and eliminate FOUC on title.

--- public/ng-app/core/Page.factory.js
+++ public/ng-app/core/Page.factory.js
@@ -6,19 +6,33 @@
 		.factory('Page', Page);
 
 	function Page() {
-		var pageTitle = '';
+		// private vars
+		var siteTitle = 'reStart-mean';
+		var pageTitle = 'Home';
 
-		function title() {
-			return pageTitle;
+		// callable members
+		return {
+			getTitle: getTitle,
+			setTitle: setTitle
+		};
+
+		/**
+		 * Title function
+		 * Sets site title and page title
+		 *
+		 * @returns {string} site title + page title
+		 */
+		function getTitle() {
+			return siteTitle + ' | ' + pageTitle;
 		}
 
+		/**
+		 * Set page title
+		 *
+		 * @param newTitle {string}
+		 */
 		function setTitle(newTitle) {
 			pageTitle = newTitle;
-		}
-
-		return {
-			title: title,
-			setTitle: setTitle
 		}
 	}
 })();
