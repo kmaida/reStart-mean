2acd5f4863483221d8dff7291650f47f6d047e65
Kim Maida <git@kim-maida.com>
2015-11-26 12:59:31 -0500

    #15, #19 - cleanup and procedural _init() for user directive.

--- public/ng-app/core/user.dir.js
+++ public/ng-app/core/user.dir.js
@@ -6,37 +6,58 @@
 		.module('myApp')
 		.directive('user', user);
 
-	user.$inject = ['userData', '$auth'];
-
-	function user(userData, $auth) {
-
-		/**
-		 * User directive controller
-		 */
-		function userCtrl() {
-			// controllerAs ViewModel
-			var u = this;
-
-			/**
-			 * Check if the current user is authenticated
-			 *
-			 * @returns {boolean}
-			 */
-			u.isAuthenticated = function() {
-				return $auth.isAuthenticated();
-			};
-
-			// API request to get the user, passing success callback function that sets the user's info
-			userData.getUser().then(function(data) {
-				u.user = data;
-			});
-		}
-
+	function user() {
+		// return directive
 		return {
 			restrict: 'EA',
 			controller: userCtrl,
 			controllerAs: 'u',
+			bindToController: true,
 			template: '<div ng-if="u.isAuthenticated() && !!u.user" class="user clearfix"><img ng-if="!!u.user.picture" ng-src="{{u.user.picture}}" class="user-picture" /><span class="user-displayName">{{u.user.displayName}}</span></div>'
 		};
+	}
+
+	userCtrl.$inject = ['userData', '$auth'];
+	/**
+	 * User directive controller
+	 */
+	function userCtrl(userData, $auth) {
+		// controllerAs ViewModel
+		var u = this;
+
+		// bindable members
+		u.isAuthenticated = _isAuthenticated;
+
+		_init();
+
+		/**
+		 * INIT function executes procedural code
+		 *
+		 * @private
+		 */
+		function _init() {
+			// API request to get the user, passing success callback function that sets the user's info
+			userData.getUser().then(_userSuccess);
+		}
+
+		/**
+		 * Check if the current user is authenticated
+		 *
+		 * @returns {boolean}
+		 * @private
+		 */
+		function _isAuthenticated() {
+			return $auth.isAuthenticated();
+		}
+
+		/**
+		 * Successful user promise
+		 *
+		 * @param data {object}
+		 * @private
+		 */
+		function _userSuccess(data) {
+			u.user = data;
+		}
 	}
 })();
