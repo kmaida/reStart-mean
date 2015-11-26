05330584d478aa0da41f878fa44de1fc42426964
Kim Maida <git@kim-maida.com>
2015-11-26 13:21:21 -0500

    #15, #19 - HeaderCtrl - bindable members up top, _init() procedural code

--- public/ng-app/header/Header.ctrl.js
+++ public/ng-app/header/Header.ctrl.js
@@ -11,20 +11,48 @@
 		// controllerAs ViewModel
 		var header = this;
 
+		// bindable members
+		header.logout = _logout;
+		header.isAuthenticated = _isAuthenticated;
+		header.indexIsActive = _indexIsActive;
+		header.navIsActive = _navIsActive;
+
+		_init();
+
+		/**
+		 * INIT function executes procedural code
+		 *
+		 * @private
+		 */
+		function _init() {
+			// get local data
+			localData.getJSON().then(_localDataSuccess);
+
+			// check if user is an admin
+			_checkUserAdmin();
+
+			// check if user is an admin on location change
+			$scope.$on('$locationChangeSuccess', _checkUserAdmin);
+		}
+
+		/**
+		 * Successfully retrieved local data
+		 *
+		 * @param data
+		 * @private
+		 */
 		function _localDataSuccess(data) {
 			header.localData = data;
 		}
 
-		localData.getJSON().then(_localDataSuccess);
-
 		/**
 		 * Log the user out of whatever authentication they've signed in with
 		 */
-		header.logout = function() {
+		function _logout() {
 			header.adminUser = undefined;
 			$auth.logout();
 			$location.path('/login');
-		};
+		}
 
 		/**
 		 * If user is authenticated and adminUser is undefined,
@@ -44,38 +72,39 @@
 					});
 			}
 		}
-		_checkUserAdmin();
-		$scope.$on('$locationChangeSuccess', _checkUserAdmin);
 
 		/**
 		 * Is the user authenticated?
 		 *
 		 * @returns {boolean}
+		 * @private
 		 */
-		header.isAuthenticated = function() {
+		function _isAuthenticated() {
 			return $auth.isAuthenticated();
-		};
+		}
 
 		/**
 		 * Currently active nav item when '/' index
 		 *
 		 * @param {string} path
 		 * @returns {boolean}
+		 * @private
 		 */
-		header.indexIsActive = function(path) {
+		function _indexIsActive(path) {
 			// path should be '/'
 			return $location.path() === path;
-		};
+		}
 
 		/**
 		 * Currently active nav item
 		 *
 		 * @param {string} path
 		 * @returns {boolean}
+		 * @private
 		 */
-		header.navIsActive = function(path) {
+		function _navIsActive(path) {
 			return $location.path().substr(0, path.length) === path;
-		};
+		}
 	}
 
 })();
