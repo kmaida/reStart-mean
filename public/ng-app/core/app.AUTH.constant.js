0aeb150b900997eada152a3cea7ab34b681ae0d5
Kim Maida <git@kim-maida.com>
2015-11-28 11:44:03 -0500

    * Minor cleanup of AUTH constant.

--- public/ng-app/core/app.AUTH.constant.js
+++ public/ng-app/core/app.AUTH.constant.js
@@ -2,19 +2,21 @@
 (function() {
 	'use strict';
 
+	var APPAUTH = {
+		LOGIN_URL: {
+			// change PROD domain to your own
+			PROD: 'http://restart-mean.kmaida.net/auth/login',
+			DEV: 'http://localhost:8081/auth/login'
+		},
+		CLIENTIDS: {
+			// change these client IDs to your own
+			FACEBOOK: '343789249146966',
+			GOOGLE: '479651367330-trvf8efoo415ie0usfhm4i59410vk3j9.apps.googleusercontent.com',
+			GITHUB: '8096e95c2eba33b81adb'
+		}
+	};
+
 	angular
 		.module('reStart-mean')
-		.constant('APPAUTH', {
-			LOGIN_URL: {
-				// change PROD domain to your own
-				PROD: 'http://restart-mean.kmaida.net/auth/login',
-				DEV: 'http://localhost:8081/auth/login'
-			},
-			CLIENTIDS: {
-				// change these client IDs to your own
-				FACEBOOK: '343789249146966',
-				GOOGLE: '479651367330-trvf8efoo415ie0usfhm4i59410vk3j9.apps.googleusercontent.com',
-				GITHUB: '8096e95c2eba33b81adb'
-			}
-		});
+		.constant('APPAUTH', APPAUTH);
 })();
